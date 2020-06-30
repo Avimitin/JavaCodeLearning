d7864720f70d7fe2397875208c57e8964838d435
avimitin <avimitin@gmail.com>
2020-06-30 16:09:54 +0800

    CoreClass Start up

---
+++ Learning/src/CoreClass.java
@@ -0,0 +1,11 @@
+package Learning;
+
+public class CoreClass{
+    public static void main(String[] args){
+        // 本章用于测试各种工具类
+        String s1 = "Hello!";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+        
+    }
+}
