6980a8bb064378099e499501cedc99987150ba95
avimitin <avimitin@gmail.com>
2020-07-01 17:38:26 +0800

    compare x and y is less than 1 and more than 0 or not

---
+++ Algorithms/src/pratise/Test1_1_5.java
@@ -0,0 +1,10 @@
+public class Test1_1_5 {
+    public static void main(String[] args){
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+
+        if (x < 1.00000 && x > 0.00000 && y < 1.00000 && y > 0.00000){
+            System.out.println("True");
+        } else System.out.println("False");
+    }
+}
