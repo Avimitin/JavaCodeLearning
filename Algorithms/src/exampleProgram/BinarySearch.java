dd35f060e0faf263581f3462ed1cdce19dc35cd0
avimitin <avimitin@gmail.com>
2020-07-01 17:38:11 +0800

    add some explaniation

--- Algorithms/src/exampleProgram/BinarySearch.java
+++ Algorithms/src/exampleProgram/BinarySearch.java
@@ -5,7 +5,9 @@ public class BinarySearch {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi){
+            // hi-lo 数组去除头尾的一半长度
             int mid = lo + (hi - lo) / 2;
+            // key 小于 数组中间值则往左半边搜索，反之则右半边
             if (key < a[mid]){
                 hi = mid - 1;
             } else if (key > a[mid]){
