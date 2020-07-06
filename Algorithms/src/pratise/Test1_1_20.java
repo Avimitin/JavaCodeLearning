e71fcef0edf4bb0c106dd966ab9e738059a388ec
avimitin <avimitin@gmail.com>
2020-07-06 17:11:04 +0800

    add new practise

---
+++ Algorithms/src/pratise/Test1_1_20.java
@@ -0,0 +1,13 @@
+public class Test1_1_20 {
+    public static void main(String[] args){
+        System.out.print(Math.log(ln(5.0)));
+    }
+
+    public static double ln(double N){
+        if (N >= 1){
+            return ln(N-1) * N;
+        } else {
+            return 1;
+        }
+    }
+}
