aa2f1d03a9026eb6561c4ce551bcb4cdca2dd80f
avimitin <avimitin@gmail.com>
2020-07-06 17:10:40 +0800

    add new practise

---
+++ Algorithms/src/pratise/Test1_1_15.java
@@ -0,0 +1,37 @@
+public class Test1_1_15 {
+    public static void main(String[] args){
+        int[] a = {1, 4, 3, 4, 7, 3, 5, 3, 8};
+        int M = 9;
+        int[] results = histogram(a, M);
+
+        int sum = 0;
+
+        for (int result : results){
+            sum += result;
+        }
+
+        if (sum == a.length){
+            System.out.print("True");
+        } else {
+            System.out.print("False");
+        }
+    }
+
+    public static int[] histogram(int[] a, int M){
+        int[] mArray = new int[M];
+        
+        for (int i = 0; i < M; i++){
+            int count = 0;
+
+            for (int num : a){
+                if (i == num){
+                    count ++;
+                }
+            }
+
+            mArray[i] = count;
+        }
+
+        return mArray;
+    }
+}
