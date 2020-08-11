f48647c7c8a7c097c1612a9180f29257ce48b9c9
Avimitin <avimitin@gmail.com>
2020-08-11 18:03:10 +0800

    fix wrong initialize of h; fix wrong array compare; add a pre load h stack;

--- Algorithms/src/pratise/ElementarySorts/ShellSort.java
+++ Algorithms/src/pratise/ElementarySorts/ShellSort.java
@@ -1,8 +1,11 @@
 package pratise.ElementarySorts;
 
 import edu.princeton.cs.algs4.StdOut;
+import programTest.Stack;
 
 public class ShellSort{
+    private Stack<Integer> stack = new Stack<>();
+
     public boolean less(Comparable x, Comparable y){
         return x.compareTo(y) < 0;
     }
@@ -22,12 +25,12 @@ public class ShellSort{
 
     public void sort(Comparable[] a){
         int len = a.length;
-        int h = 0;
+        int h = 1;
         while (h < len/3) h = 3*h+1;
         while (h >= 1){
             for (int i = h; i < len; i++){
                 // 把 j 起始值设置在最末端i，每次 -h 往前比对直到达到最小的间隔角标 h
-                for (int j = i; j >= h && less(j, j-h); j-=h){
+                for (int j = i; j >= h && less(a[j], a[j-h]); j-=h){
                     exchange(a, j, j-h);
                 }
             }
@@ -35,6 +38,26 @@ public class ShellSort{
         }
     }
     
+    public void preStoreSort(Comparable[] a){
+        int len = a.length;
+        int h = 1;
+        stack.push(h);
+        while (h < len/3){
+            h = 3*h +1;
+            stack.push(h);
+        }
+        
+        h = stack.pop();
+        while (h >=1){
+            for (int i =h; i <len; i++){
+                for (int j = i; j>=h && less(a[j], a[j-h]); j-=h){
+                    exchange(a, j, j-h);
+                }
+            }
+            h = stack.pop();
+        }
+    }
+
     public static void main(String[] args){
         ShellSort s = new ShellSort();
         boolean a = s.less('g', 'c');
