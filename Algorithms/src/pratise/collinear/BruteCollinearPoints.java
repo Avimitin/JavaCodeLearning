fb6e1b49b5973dae69a0015b3fa525fe39aee6c4
Avimitin <avimitin@gmail.com>
2020-08-27 19:06:54 +0800

    增加测试用例，修改类属性，增加输入判断

--- Algorithms/src/pratise/collinear/BruteCollinearPoints.java
+++ Algorithms/src/pratise/collinear/BruteCollinearPoints.java
@@ -1,35 +1,45 @@
 package pratise.collinear;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
     // lineSegment 是从起点到终点的线段，lineSegments 用于存储符合条件的线段
-    private LineSegment[] lineSegments;
-    private Point[] pointsCopy;
-    private int len;
+    private final LineSegment[] lineSegments;
+    private int num = 0;
 
-    public BruteCollinearPoints(Point[] points){
+    public BruteCollinearPoints(Point[] points) {
+        if ((points == null) || isDuplicated(points)) throw new IllegalArgumentException();
+
         // 初始化线段数组
-        len = points.length;
-        lineSegments = new LineSegment[len-3];
-        pointsCopy = new Point[len];
-        System.arraycopy(points, 0, pointsCopy, 0, len);
+        int len = points.length;
+        lineSegments = new LineSegment[len -3];
+        Point[] pointsCopy = new Point[len];
+
+        for (int i = 0; i < len; i++) {
+            if (points[i] == null) throw new IllegalArgumentException();
+            pointsCopy[i] = points[i];
+        }
+
         Arrays.sort(pointsCopy);
         // 从头开始遍历，选择第一个节点与后面三个节点比较，4个4个暴力比较
-        for (int i = 0; i< len-3; i++){
-            for (int j = i+1; j < len-2;j++) {
+        for (int i = 0; i < len -3; i++) {
+            for (int j = i+1; j < len -2; j++) {
                 // slope 是起点到下一个点的斜率
                 double slope = pointsCopy[i].slopeTo(pointsCopy[j]);
-                for (int k = j+1; k < len-1; k++){
+                for (int k = j+1; k < len -1; k++) {
                     // 当第三个点到第二个点的斜率不等于第二个点到起点的斜率时，
                     // 说明三点不在一条线上，直接跳过。
-                    if (pointsCopy[k].slopeTo(pointsCopy[j]) != slope) continue;
-                    for (int l = k+1; l<pointsCopy.length; l++){
+                    if (!(isEqual(pointsCopy[k].slopeTo(pointsCopy[j]), slope))) continue;
+                    for (int x = k+1; x < len; x++) {
                         // 前面三点已经确定在一条线上了，
                         // 只要终点到第三个点的斜率与起点到第二个点的斜率相等
                         // 则说明4点同线，把起点到终点的线段 lineSegment 存到数组里
-                        if (pointsCopy[l].slopeTo(pointsCopy[k]) == slope){
-                            lineSegments[i] = new LineSegment(pointsCopy[i], pointsCopy[l]);
+                        if (isEqual(pointsCopy[x].slopeTo(pointsCopy[k]), slope)) {
+                            lineSegments[num++] = new LineSegment(pointsCopy[i], pointsCopy[x]);
                         }
                     }
                 }
@@ -37,11 +47,54 @@ public class BruteCollinearPoints {
         }
     }
 
-    public int numberOfSegments(){
-        return lineSegments.length;
+    public int numberOfSegments() {
+        return num;
     }
 
-    public LineSegment[] segments(){
-        return lineSegments;
+    public LineSegment[] segments() {
+        LineSegment[] segmentsCopy = new LineSegment[lineSegments.length];
+        for (int i = 0; i < lineSegments.length; i++) segmentsCopy[i] = lineSegments[i];
+        return segmentsCopy;
+    }
+
+    private boolean isDuplicated(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i] == points[i+1]) return true;
+        }
+        return false;
+    }
+
+    private boolean isEqual(double x, double y) {
+        return Double.compare(x, y) == 0;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
